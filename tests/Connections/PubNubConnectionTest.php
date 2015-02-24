822fe0bb32ea706b65a4add7bdb006c2858054b1
duxet <duxetlg@gmail.com>
2015-02-24 14:42:59 +0100

    Add subscribe() test to PubNub

--- tests/Connections/PubNubConnectionTest.php
+++ tests/Connections/PubNubConnectionTest.php
@@ -11,6 +11,7 @@ class PubNubConnectionTest extends TestCase {
         $pubnub = $this->getPubNub()
             ->shouldReceive('publish')
             ->with('channel', 'test')
+            ->once()
             ->andReturn([1, 'Sent', 14247819253237675])
             ->mock();
 
@@ -18,6 +19,29 @@ class PubNubConnectionTest extends TestCase {
         $result = $connection->publish('channel', 'test');
 
         $this->assertTrue($result);
+    }
+
+    public function testSubscribe()
+    {
+        $result = null;
+        $closure = function($message) use(&$result)
+        {
+            $result = $message;
+        };
+
+        $pubnub = $this->getPubNub()
+            ->shouldReceive('subscribe')
+            ->with('channel', Mockery::type('callable'))
+            ->andReturnUsing(function($channel, $callback) {
+                $callback('test');
+            })
+            ->once()
+            ->mock();
+
+        $connection = $this->getConnection($pubnub);
+        $connection->subscribe('channel', $closure);
+
+        $this->assertEquals($result, 'test');
     }
 
     protected function getConnection($pubnub)
