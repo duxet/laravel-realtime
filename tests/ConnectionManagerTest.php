c00bfbb28aea924deb79d8271daac9e7fc097bec
duxet <duxetlg@gmail.com>
2015-02-24 01:19:48 +0100

    Add Connection Manager test

---
+++ tests/ConnectionManagerTest.php
@@ -0,0 +1,43 @@
+<?php namespace duxet\Tests\Realtime; 
+
+use duxet\Realtime\ConnectionManager;
+use duxet\Realtime\Connections\NullConnection;
+use Mockery;
+
+class ConnectionManagerTest extends TestCase {
+
+    public function testConnectionName()
+    {
+        $config = ['driver' => 'null'];
+        $manager = $this->getConfigManager($config);
+        $this->assertSame([], $manager->getConnections());
+        $return = $manager->connection('local');
+        $this->assertInstanceOf('duxet\Realtime\Contracts\Connection', $return);
+        $this->assertArrayHasKey('local', $manager->getConnections());
+    }
+
+    protected function getManager()
+    {
+        $config = Mockery::mock('Illuminate\Contracts\Config\Repository');
+        $manager = new ConnectionManager($config);
+
+        $manager->extend('null', function() {
+           return new NullConnection;
+        });
+
+        return $manager;
+    }
+
+    protected function getConfigManager(array $config)
+    {
+        $manager = $this->getManager();
+
+        $manager->getConfig()->shouldReceive('get')->once()
+            ->with('realtime.connections')->andReturn(['local' => $config]);
+
+        $config['name'] = 'local';
+
+        return $manager;
+    }
+
+}
